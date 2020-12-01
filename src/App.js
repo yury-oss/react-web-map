dc95f943a33f7166137e6ff0448fad5270a9f879
yury-oss <infocod4@yandex.ru>
2020-12-01 21:52:50 +0300

    leaflet-topography_connected

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './style.css';
 import L from 'leaflet';
-import   'leaflet/dist/leaflet.css';
+import 'leaflet/dist/leaflet.css';
+import React from 'react';
+
 
 function App() {
   return (
-  //  <div className="App"> hi reeact
-    // </div>
+     // <div className="App"> hi reeact
+              
+   
+     //</div>
   
-<div id="mapid" >
-
-
-</div>
+      <div id="mapid" ></div>
+      
   );
 }
 
