7a6d73bc487622f245ef5b0e781cb04f9a5e9812
yury-oss <infocod4@yandex.ru>
2020-12-01 16:58:21 +0300

    starting prj

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './style.css';
+import L from 'leaflet';
+import   'leaflet/dist/leaflet.css';
+
+function App() {
+  return (
+  //  <div className="App"> hi reeact
+    // </div>
+  
+<div id="mapid" >
+
+
+</div>
+  );
+}
+
+export default App;
